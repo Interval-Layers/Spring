e67d7aec16ae6cf932aaac9da7a6e10b6eef9b88
Nourepide <nourepide@gmail.com>
2020-04-01 17:13:19 +0300

    Reformat build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,35 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.5.RELEASE"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	war
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
+	id("org.springframework.boot") version "2.2.5.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 }
 
 group = "net.intervallayers"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.91"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 val developmentOnly: Configuration by configurations.creating
+
 configurations {
 	runtimeClasspath {
 		extendsFrom(developmentOnly)
 	}
 }
+
 repositories {
 	mavenCentral()
 	jcenter()
 }
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-//	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -40,9 +42,11 @@ dependencies {
 	}
 	testImplementation("io.projectreactor:reactor-test")
 }
+
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
