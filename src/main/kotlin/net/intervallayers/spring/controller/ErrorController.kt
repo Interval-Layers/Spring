dda5e999a534855f4c4ee8c8d03e7fe9dadf1ccf
Nourepide <nourepide@gmail.com>
2020-04-02 00:52:05 +0300

    Add Error.kt and errorPage

--- src/main/kotlin/net/intervallayers/spring/controller/ErrorController.kt
+++ src/main/kotlin/net/intervallayers/spring/controller/ErrorController.kt
@@ -1,9 +1,6 @@
 package net.intervallayers.spring.controller
 
-import kotlinx.html.*
-import kotlinx.html.stream.*
-import net.intervallayers.spring.model.html.*
-import net.intervallayers.spring.model.html.element.*
+import net.intervallayers.spring.view.page.*
 import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.stereotype.*
 import org.springframework.web.bind.annotation.*
@@ -16,30 +13,5 @@ class ErrorController : ErrorController {
 
     @ResponseBody
     @RequestMapping("/error")
-    fun handleError(request: HttpServletRequest) = createHTML().htmlWithDoctype {
-        head {
-            title("Error page")
-            metaHeadConstructor()
-            linkHeadConstructor()
-        }
-        body {
-            headerBodyConstructor()
-            div(classes = "body-container") {
-                h1("title") {
-                    text("An error has occurred")
-                }
-                h1("title") {
-                    when (val type = request.getAttribute("javax.servlet.error.status_code") as? Int) {
-                        null -> text("Type: Undefined")
-                        else -> text("Type: $type")
-                    }
-                }
-                h3 {
-                    text("Method: " + request.method)
-                }
-                a(href = "/", classes = "button-container") { text("Return to the main page ->>") }
-            }
-            footerBodyConstructor()
-        }
-    }
+    fun handleError(request: HttpServletRequest) = errorPage(request)
 }
