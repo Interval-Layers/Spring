01594cc1e66780c1431cedf519d3fc582a0f94d5
Nourepide <Nourepide@gmail.com>
2020-04-21 00:20:03 +0400

    Add onLoad in FooterElement.kt

--- src/main/kotlin/net/intervallayers/spring/view/element/FooterElement.kt
+++ src/main/kotlin/net/intervallayers/spring/view/element/FooterElement.kt
@@ -2,10 +2,11 @@ package net.intervallayers.spring.view.element
 
 import kotlinx.html.*
 import net.intervallayers.extensions.html.*
+import net.intervallayers.extensions.string.*
 import java.text.*
 import java.util.*
 
-fun BODY.footerElement(tags: (FOOTER.() -> Unit) = {}) {
+fun BODY.footerElement(onLoad: String = "", tags: (FOOTER.() -> Unit) = {}) {
     footer {
         section(classes = "footer-container") {
             p { text("product by: nourepide") }
@@ -15,5 +16,6 @@ fun BODY.footerElement(tags: (FOOTER.() -> Unit) = {}) {
         tags()
 
         script(src = "/javascript/main.js")
+        onLoad.ifNotBlank { script { unsafe { raw("window.onload = $onLoad") } } }
     }
 }
