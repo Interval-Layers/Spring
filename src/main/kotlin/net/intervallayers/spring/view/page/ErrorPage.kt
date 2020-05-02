cda5db419d6012af6928bb6567fd6c82b2860238
Nourepide <Nourepide@gmail.com>
2020-05-02 22:04:04 +0400

    Rework ErrorPage.kt

--- src/main/kotlin/net/intervallayers/spring/view/page/ErrorPage.kt
+++ src/main/kotlin/net/intervallayers/spring/view/page/ErrorPage.kt
@@ -1,6 +1,7 @@
 package net.intervallayers.spring.view.page
 
 import kotlinx.html.*
+import org.springframework.http.*
 import org.springframework.stereotype.*
 import javax.servlet.http.*
 
@@ -8,25 +9,31 @@ import javax.servlet.http.*
 class ErrorPage : GenericPage() {
 
     private lateinit var request: HttpServletRequest
+    private val status by lazy {
+        HttpStatus
+            .valueOf(request.getAttribute("javax.servlet.error.status_code") as? Int ?: 400)
+    }
 
     fun setRequest(request: HttpServletRequest) = also { it.request = request }
 
     override fun BODY.main() {
         main {
             section(classes = "main-container") {
-                h1("title") {
-                    text("An error has occurred")
-                }
-                h1("title") {
-                    when (val type = request.getAttribute("javax.servlet.error.status_code") as? Int) {
-                        null -> text("Type: Undefined")
-                        else -> text("Type: $type")
+                h1("title") { text("An error has occurred") }
+
+                article("card") {
+                    h3 { text("Information") }
+                    p { text("Code: " + status.value()) }
+                    p { text("Message: " + status.reasonPhrase) }
+                    p {
+                        text("Status: ")
+                        span("apply-red") { text("Error") }
+                    }
+                    p { text("Method: " + request.method) }
+                    a(href = "/", classes = "form-button apply-margin-top-8px") {
+                        text("Return to the main page")
                     }
                 }
-                h3 {
-                    text("Method: " + request.method)
-                }
-                a(href = "/", classes = "form-button apply-margin-top-8px") { text("Return to the main page") }
             }
         }
     }
