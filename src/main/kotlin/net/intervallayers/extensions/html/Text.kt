40c617b1be1a3770b645aad415ba0f1ea9483e77
Nourepide <Nourepide@gmail.com>
2020-05-02 23:45:54 +0400

    Add extension tag text with classes support

--- src/main/kotlin/net/intervallayers/extensions/html/Text.kt
+++ src/main/kotlin/net/intervallayers/extensions/html/Text.kt
@@ -6,3 +6,8 @@ import kotlinx.html.*
 fun Tag.text(s: Any?) {
     consumer.onTagContent(s.toString())
 }
+
+@HtmlTagMarker
+fun FlowOrPhrasingContent.text(s: Any?, classes: String = "") {
+    span(classes) { (this as Tag).text(s) }
+}
