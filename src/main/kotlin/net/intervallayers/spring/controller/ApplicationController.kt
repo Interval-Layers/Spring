84e8c82fed4e75c4548af355c8da5693fc6e1067
Nourepide <Nourepide@gmail.com>
2020-05-06 00:50:13 +0400

    Delete InsertPage.kt

--- src/main/kotlin/net/intervallayers/spring/controller/ApplicationController.kt
+++ src/main/kotlin/net/intervallayers/spring/controller/ApplicationController.kt
@@ -1,15 +1,10 @@
 package net.intervallayers.spring.controller
 
 import net.intervallayers.spring.*
-import net.intervallayers.spring.model.*
 import net.intervallayers.spring.view.page.*
 import org.springframework.beans.factory.annotation.*
-import org.springframework.http.*
 import org.springframework.stereotype.*
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.*
-import java.text.*
-import java.util.*
 
 @Controller
 class ApplicationController {
@@ -20,22 +15,6 @@ class ApplicationController {
     @ResponseBody
     @RequestMapping("/")
     fun index(page: IndexPage) = page.setSizeOfEntities(entityRepository.size).document
-
-    @ResponseBody
-    @RequestMapping("/insert")
-    fun insert(@RequestParam name: String, page: InsertPage): String {
-        if (name.isBlank())
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
-
-        return page.setEntity {
-            EntityBuilder()
-                .setId(entityRepository.size.toString())
-                .setTime(SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()))
-                .setName(name)
-                .build()
-                .insert(entityRepository)
-        }.document
-    }
 
     @ResponseBody
     @RequestMapping("/entity")
