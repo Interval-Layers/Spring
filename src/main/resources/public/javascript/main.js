29bbd9ac9f661387b3483695d52b5ccc4486c78a
Nourepide <Nourepide@gmail.com>
2020-05-05 23:57:04 +0400

    Add capitalize function to main.js

--- src/main/resources/public/javascript/main.js
+++ src/main/resources/public/javascript/main.js
@@ -49,3 +49,7 @@ function toggleWindowLogin() {
 function toggleWindowRegistration() {
     WindowHandler.object.toggleWindow(document.getElementById("windowRegistration"))
 }
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
