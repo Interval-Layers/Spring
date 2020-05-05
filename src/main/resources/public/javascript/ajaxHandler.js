dcafd8d5096d8e31947b2a318a571357095dd00c
Nourepide <Nourepide@gmail.com>
2020-05-06 00:35:05 +0400

    Add AjaxPostHandler

---
+++ src/main/resources/public/javascript/ajaxHandler.js
@@ -0,0 +1,12 @@
+class AjaxPostHandler {
+    constructor(data, url, f) {
+        this.request = new XMLHttpRequest()
+        this.request.addEventListener("readystatechange", () => {
+            f(this.request)
+        })
+        this.request.open("POST", url)
+        this.request.setRequestHeader( "Content-Type", "application/json" );
+        this.request.responseType = "json"
+        this.request.send(data)
+    }
+}
