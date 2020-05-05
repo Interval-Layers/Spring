4bc11de98ee4af6db3e2ed95637ab9ad3cf7fed2
Nourepide <Nourepide@gmail.com>
2020-05-05 20:42:42 +0400

    Add API for insert Entity with name

--- src/main/kotlin/net/intervallayers/spring/controller/APIController.kt
+++ src/main/kotlin/net/intervallayers/spring/controller/APIController.kt
@@ -1,8 +1,13 @@
 package net.intervallayers.spring.controller
 
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.node.*
 import net.intervallayers.spring.*
+import net.intervallayers.spring.model.*
 import org.springframework.beans.factory.annotation.*
 import org.springframework.web.bind.annotation.*
+import java.text.*
+import java.util.*
 
 @RestController
 class APIController {
@@ -20,4 +25,20 @@ class APIController {
     @RequestMapping("/api/entity/{name}", method = [RequestMethod.GET])
     fun getEntityByName(@PathVariable name: String) =
         entityRepository.list.filter { it.name == name }
+
+    @PostMapping("/api/insert/entity")
+    fun insertEntity(@RequestBody entityName: String): ObjectNode {
+        val entity = EntityBuilder()
+            .setId(entityRepository.size.toString())
+            .setTime(SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()))
+            .setName(entityName)
+            .build()
+            // Temporally disabled
+            //.insert(entityRepository)
+
+        return ObjectMapper()
+            .createObjectNode()
+            .put("status", "success")
+            .putPOJO("entity", entity)
+    }
 }
