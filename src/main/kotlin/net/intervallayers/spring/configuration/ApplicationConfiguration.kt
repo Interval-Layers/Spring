3a11b45a8a786a798a1ffbe91cba008a0130b90e
Nourepide <nourepide@gmail.com>
2020-05-21 21:01:57 +0400

    Delete entity bean from ApplicationConfiguration.kt

--- src/main/kotlin/net/intervallayers/spring/configuration/ApplicationConfiguration.kt
+++ src/main/kotlin/net/intervallayers/spring/configuration/ApplicationConfiguration.kt
@@ -1,11 +1,6 @@
 package net.intervallayers.spring.configuration
 
-import net.intervallayers.spring.model.*
-import org.springframework.context.annotation.*
 import org.springframework.stereotype.*
 
 @Component
-class ApplicationConfiguration {
-    @Bean
-    private fun entity() = Entity("", "", "")
-}
+class ApplicationConfiguration
