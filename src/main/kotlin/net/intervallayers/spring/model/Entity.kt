928647d48d1f71359f33190a4f0251c00e4c8b4e
Nourepide <nourepide@gmail.com>
2020-05-27 19:11:04 +0400

    Generate new Entity id if it is Null

--- src/main/kotlin/net/intervallayers/spring/model/Entity.kt
+++ src/main/kotlin/net/intervallayers/spring/model/Entity.kt
@@ -4,8 +4,4 @@ import org.bson.types.*
 import org.springframework.data.mongodb.core.mapping.*
 
 @Document("entity")
-data class Entity(@Field val name: String) {
-
-    @MongoId
-    lateinit var id: ObjectId
-}
+data class Entity(@Field val name: String, @MongoId val id: ObjectId = ObjectId())
